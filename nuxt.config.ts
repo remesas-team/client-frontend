afb4050bd66bbd264a856b17966dd4f64fc280d6
dilverdev <dilvermg@gmail.com>
2025-02-15 19:56:11 -0500

    feat: enable devtools

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   ssr: true,
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: false },
+  devtools: { enabled: true },
   modules: [
     '@nuxtjs/tailwindcss',
     '@pinia/nuxt',
