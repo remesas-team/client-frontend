9e339c22127f3bc7a60d1b68ef5c37fc47ec20bc
dilverdev <dilvermg@gmail.com>
2025-02-18 09:50:22 -0500

    feat: add setHeader to $api

--- plugins/01.api.ts
+++ plugins/01.api.ts
@@ -1,12 +1,18 @@
 export default defineNuxtPlugin(({ provide, $config }) => {
+	const commonHeaders = new Headers({
+		'Content-Type': 'application/json',
+		'Accept': 'application/json'
+	})
+
 	const $api = $fetch.create({
 		baseURL: $config.public.apiBase,
-		onRequest({ request, options }) {
-			options.headers.set('Content-Type', 'application/json')
-			options.headers.set('Accept', 'application/json')
+		onRequest({ options }) {
+			commonHeaders.forEach((value, key) => {
+				options.headers.set(key, value)
+			})
 		},
-		async onResponse({ response}) {
-		},
+		// async onResponse({ response}) {
+		// },
 		async onResponseError({ response }) {
 			// if (response.status === 401) {
 			// interceptar error de 401
@@ -15,6 +21,8 @@ export default defineNuxtPlugin(({ provide, $config }) => {
 			throw response
 		},
 	})
+
+	$api.setHeader = (key: string, value: string) => commonHeaders.set(key, value)
 
 	provide('api', $api)
 })
