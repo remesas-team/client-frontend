0710a2af23a8a2e88ae140f633dd9ed18a88b386
dilverdev <dilvermg@gmail.com>
2025-02-18 09:47:59 -0500

    feat: remove store system state

--- app.vue
+++ app.vue
@@ -9,12 +9,4 @@
 </template>
 
 <script setup lang="ts">
-import { useSystemConfigStore } from '~/stores/systemConfig';
-
-const systemConfig = useSystemConfigStore();
-
-// Initialize system configuration when app starts
-onMounted(async () => {
-  await systemConfig.initializeConfig();
-});
 </script>
