8fe6d3bbdaf12910e8a22665f91dbc1c1b7a40af
dilverdev <dilvermg@gmail.com>
2025-02-22 18:11:01 -0500

    feat: transform objeto to array

--- components/wizard/WizardStepRecipient.vue
+++ components/wizard/WizardStepRecipient.vue
@@ -9,7 +9,7 @@
           <div class="mb-2" v-if="savedAccounts.length">
             <label for="">Destinatarios guardados:</label>
             <USelect v-model="form.bank" :items="savedAccounts" value-attribute="id" placeholder="Selecciona un banco"
-              size="xl" class="w-full text-xl" />
+                     size="xl" class="w-full text-xl"/>
           </div>
         </div>
       </div>
@@ -18,22 +18,22 @@
       <div class="w-full mb-2">
         <label for="">Nuevo destinatario:</label>
         <USelect v-model="form.bank" :items="banks" value-attribute="id" placeholder="Selecciona un banco" size="xl"
-          class="w-full text-xl" />
+                 class="w-full text-xl"/>
       </div>
       <div class="w-full mb-2">
         <UInput v-model="form.accountNumber" type="text" pattern="[0-9]*" inputmode="numeric"
-          placeholder="Número de cuenta interbancario" size="xl" class="w-full text-xl"
-          @input="form.accountNumber = form.accountNumber.replace(/\D/g, '')" />
+                placeholder="Número de cuenta interbancario" size="xl" class="w-full text-xl"
+                @input="form.accountNumber = form.accountNumber.replace(/\D/g, '')"/>
       </div>
       <div class="w-full">
         <UInput v-model="form.recipientName" type="text" placeholder="Nombre del destinatario" size="xl"
-          class="w-full text-xl" />
+                class="w-full text-xl"/>
       </div>
 
       <div class="mt-6">
         <label class="flex items-center">
           <input v-model="form.saveAccount" type="checkbox"
-            class="rounded border-gray-300 text-blue-600 shadow-sm focus:border-blue-500 focus:ring-blue-500" />
+                 class="rounded border-gray-300 text-blue-600 shadow-sm focus:border-blue-500 focus:ring-blue-500"/>
           <span class="ml-2 text-sm text-gray-600">
             Guardar como cuenta frecuente
           </span>
@@ -43,7 +43,7 @@
       <!-- Save Account Name -->
       <div v-if="form.saveAccount" class="mt-4 w-full">
         <UInput v-model="form.accountAlias" type="text" placeholder="Nombre del destinatario" size="xl"
-          class="w-full text-xl" />
+                class="w-full text-xl"/>
       </div>
 
       <!-- Submit Button -->
@@ -56,12 +56,18 @@
 </template>
 
 <script setup lang="ts">
-import { userRepository } from "~/repositories/v1/platform/userRepository"
-import { useRemittanceStore } from '~/stores/remittance'
+import {userRepository} from "~/repositories/v1/platform/userRepository"
+import {useRemittanceStore} from '~/stores/remittance'
+import {sourcesRepository} from "~/repositories/v1/platform/sourcesRepository";
 
 const userRepo = userRepository()
-const sourcesStore = useSourcesStore()
 const remittanceStore = useRemittanceStore()
+const requestSources = sourcesRepository();
+
+const emit = defineEmits<{
+  (e: 'next'): void;
+}>();
+
 const banks = ref([])
 const savedAccounts = ref([])
 const form = ref({
@@ -72,15 +78,7 @@ const form = ref({
   accountAlias: ''
 })
 
-const isFormValid = computed(() => {
-  return form.bank && form.accountNumber && form.recipientName;
-});
-
 const handleSubmit = async () => {
-
-  // Here you can handle the form submission
-  // For example, save to store or emit event
-
   const bank_id = form.value.bank
 
   const response = await userRepo.createBankAccount({
@@ -100,21 +98,20 @@ const handleSubmit = async () => {
   emit('next');
 };
 
-const emit = defineEmits<{
-  (e: 'next'): void;
-}>();
 
-
-// Add onMounted hook
 onMounted(async () => {
-  //Deuda: Se debe obtener el pais de la orden de remesa sin perderlo al actualizar
-  const banksByCountry = await sourcesStore.fetchBanksByCountry(remittanceStore.form.source_country_id ? remittanceStore.form.source_country_id : 1); // You might want to pass the actual country_id
-  const destinataryBanksAccounts = await userRepo.fetchBankAccounts();
+  const response = await requestSources.getBanksByCountry({country_id: remittanceStore.form.source_country_id});
 
-  banks.value = Object.entries(banksByCountry.data.banks).map(([id, name]) => ({
-    value: id,
-    label: name
-  }));
+  if (Array.isArray(response.data.banks)) {
+    banks.value = response.data.banks
+  } else {
+    banks.value = Object.entries(response.data.banks).map(([id, name]) => ({
+      value: id,
+      label: name
+    }));
+  }
+
+  const destinataryBanksAccounts = await userRepo.fetchBankAccounts();
 
   savedAccounts.value = destinataryBanksAccounts.data.map((item) => {
     return {
@@ -123,8 +120,6 @@ onMounted(async () => {
     }
   })
 
-  console.log(savedAccounts.value);
-  console.log(banks.value);
 });
 
 </script>
