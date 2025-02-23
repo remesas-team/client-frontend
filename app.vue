92126bdbb409fd4358eedb81fa612e986d8305c5
cristian <cristian@remesas.com>
2025-02-22 19:15:09 -0500

    metas

--- app.vue
+++ app.vue
@@ -9,4 +9,24 @@
 </template>
 
 <script setup lang="ts">
+  useSeoMeta({
+    title: 'Remesas.com',
+    description: '[description]',
+    ogTitle: '[og:title]',
+    ogDescription: '[og:description]',
+    ogImage: '/thumbnail.png',
+  })
+
+  useHead({
+    htmlAttrs: {
+      lang: 'en'
+    },
+    link: [
+      {
+        rel: 'icon',
+        type: 'image/png',
+        href: '/favicon.png'
+      }
+    ]
+  })
 </script>
