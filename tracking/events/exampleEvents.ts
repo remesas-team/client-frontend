834ffed4103b8edf821d864a69793b95ad924671
dilverdev <dilvermg@gmail.com>
2025-03-27 13:21:32 -0500

    feat: add use example event

--- tracking/events/exampleEvents.ts
+++ tracking/events/exampleEvents.ts
@@ -1,5 +1,24 @@
 import tracer from '~/tracking/analytics'
+// import { useRemittanceStore } from '~/stores/remittance'
 
-export const trackExample = () => {
-	tracer.track('example')
+export const trackExample = (params = {}) => {
+	// Access the Nuxt app
+	// const { $config } = useNuxtApp()
+
+	// Access the route
+	// const route = useRoute()
+
+	// Access the store
+	// const remittanceStore = useRemittanceStore()
+
+	// Access the cookie
+	// const cookie = useCookie('')
+
+	tracer.track('example', {
+		name: params?.name,
+		// page: route.path,
+		// url: $config.public.domain,
+		// cookie: cookie.value,
+		// store: remittanceStore.form,
+	})
 }
